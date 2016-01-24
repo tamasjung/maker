9cfce2033da943fa63d565a0c3bc7767148385f0
Tamas Jung <tamasjung@gmail.com>
2016-01-24 21:50:22 +0100

    release 0.1.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject maker "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC3"]]
+(defproject maker "0.1.0"
+  :description "Maker is a macro for resolving dependencies between plain functions."
+  :url "https://github.com/tamasjung/maker"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :source-paths ["src"]
   :profiles {:dev [:dev-common :dev-local]
              :dev-common {:plugins [[org.clojure/tools.nrepl "0.2.12"
