95c1c7e9200e3c8e32786ad5c855d4ea0c84bc2f
Tamas Jung <tamasjung@gmail.com>
2017-04-04 23:03:22 +0200

    clean up

--- project.clj
+++ project.clj
@@ -4,6 +4,4 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :source-paths ["src"]
-  :profiles {:dev [:dev-common :dev-local]
-             :dev-common {}})
+  :source-paths ["src"])
