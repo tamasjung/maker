21e319b48a20abfc364c4c1f10dd07c5bfa667bf
Tamas Jung <tamasjung@gmail.com>
2019-10-20 22:09:56 +0100

    release 2.1.0,

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject maker "2.0.3-SNAPSHOT"
+(defproject maker "2.1.0"
   :description "Clojure library to explore inversion of control technique - in several senses."
   :url "https://github.com/tamasjung/maker"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/core.async "0.4.490"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.async "0.4.500"]]
   :source-paths ["src"]
-  :profiles {:dev {:dependencies [[criterium "0.4.4"]
-                                  #_[org.clojure/test.check "0.9.0"]]}})
+  :profiles {:dev {:dependencies [[criterium "0.4.5"]]}})
