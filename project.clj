08923e81ff91a3984c079ac2e168cc2af0a48de8
Tamas Jung <tamasjung@gmail.com>
2019-10-20 22:31:07 +0100

    change version to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject maker "2.1.0"
+(defproject maker "2.1.1-SNAPSHOT"
   :description "Clojure library to explore inversion of control technique - in several senses."
   :url "https://github.com/tamasjung/maker"
   :license {:name "MIT License"
@@ -6,4 +6,6 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "0.4.500"]]
   :source-paths ["src"]
-  :profiles {:dev {:dependencies [[criterium "0.4.5"]]}})
+  :profiles {:dev {:dependencies [[criterium "0.4.5"]]}}
+  :deploy-repositories {"releases" {:url "https://repo.clojars.org" :creds :gpg
+                                    :sign-releases false}})
